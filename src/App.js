aa3a0cd45cd6c566a76c77b7673821e1b80871d0
Muthoni Njoki <fmuthoninjoki@gmail.com>
2024-01-01 23:55:28 +0300

    My first 2024 commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+import { useState } from 'react';
+import AddItem from './AddItem';
+
+function App() {
+    const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
+
+  const [newItem, setNewItem] = useState('')
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(newItems));
+  }
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item};
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  }
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+    setAndSaveItems(listItems);
+  }
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setAndSaveItems(listItems);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem('');
+  }
+
+  return (
+    <div className="App">
+      <Header title="Groceries List" ></Header>
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      ></AddItem>
+      <Content
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      ></Content>
+      <Footer length={items.length}></Footer>
+    </div>
+  );
+}
+
+export default App;
